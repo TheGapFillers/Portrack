7aaa890acdf491434ddae53ae4245a8b953c0474
Ambroise Couissin <ambroise.couissin@gmail.com>
2015-01-13 04:08:54 +0800

    Repackaging of the whole solution

--- src/PortTracker/PortTracker/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace PortTracker.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- src/PortTracker/PortTracker/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace PortTracker.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
