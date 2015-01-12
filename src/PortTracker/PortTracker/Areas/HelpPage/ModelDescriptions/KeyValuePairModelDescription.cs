475b5f4664dc7023c98c1dc90df0cc8a4a12fc31
Ambroise Couissin <ambroise.couissin@gmail.com>
2015-01-13 02:46:27 +0800

    First commit.

---
+++ src/PortTracker/PortTracker/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace PortTracker.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
