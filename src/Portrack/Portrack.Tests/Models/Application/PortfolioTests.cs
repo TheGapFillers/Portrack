da4e3131639d3055eee909ff78f271c91f8a46e6
Ambroise Couissin <ambroise.couissin@gmail.com>
2015-02-20 18:40:35 +0800

    A few tweeks. Not much

--- src/Portrack/Portrack.Tests/Models/Application/PortfolioTests.cs
+++ src/Portrack/Portrack.Tests/Models/Application/PortfolioTests.cs
@@ -7,68 +7,68 @@ using Portrack.Tests.Repositories;
 
 namespace Portrack.Tests.Models.Application
 {
-    [TestClass]
-    public class PortfolioTests
-    {
-        private Portfolio _portfolio;
-        private readonly TestData _testData = new TestData();
+	[TestClass]
+	public class PortfolioTests
+	{
+		private Portfolio _portfolio;
+		private readonly TestData _testData = new TestData();
 
-        [TestInitialize]
-        public void Initialize()
-        {
-            _portfolio = new Portfolio
-            {
-                PortfolioName = "PortfolioTest",
-                UserName = "UserTest",
-                Positions = new List<Position>
-                {
-                    new Position(_portfolio, _testData.Instruments.SingleOrDefault(i => i.Ticker == "YHOO"), 4),
-                    new Position(_portfolio, _testData.Instruments.SingleOrDefault(i => i.Ticker == "GOOG"), 6),
-                },
-                Transactions = new List<Transaction>()
-            };
-        }
+		[TestInitialize]
+		public void Initialize()
+		{
+			_portfolio = new Portfolio
+			{
+				PortfolioName = "PortfolioTest",
+				UserName = "UserTest",
+				Positions = new List<Position>
+				{
+					new Position(_portfolio, _testData.Instruments.SingleOrDefault(i => i.Ticker == "YHOO"), 4),
+					new Position(_portfolio, _testData.Instruments.SingleOrDefault(i => i.Ticker == "GOOG"), 6),
+				},
+				Transactions = new List<Transaction>()
+			};
+		}
 
 
-        [TestMethod]
-        public void CheckSellNonExistingPositionError()
-        {
-            var transactionToAdd = new Transaction { Ticker = "MSFT", Shares = 1, Type = TransactionType.Sell };
+		[TestMethod]
+		public void CheckSellNonExistingPositionError()
+		{
+			var transactionToAdd = new Transaction { Ticker = "MSFT", Shares = 1, Type = TransactionType.Sell };
 
-            TransactionResult result = _portfolio.AddTransaction(
-                transactionToAdd, 
-                null
-            );
+			TransactionResult result = _portfolio.AddTransaction(
+				transactionToAdd, 
+				null
+			);
 
-            Assert.AreEqual(false, result.IsSuccess);
-            Assert.AreEqual("Cannot work on a non-existing position.", result.Errors.FirstOrDefault());
-        }
+			Assert.AreEqual(false, result.IsSuccess);
+			Assert.AreEqual("Cannot work on a non-existing position.", result.Errors.FirstOrDefault());
+		}
 
 		[TestMethod]
 		public void CheckBuyNonExistingPositionError()
-        {
-            var transactionToAdd = new Transaction { Ticker = "MSFT", Shares = 1, Type = TransactionType.Buy };
+		{
+			var transactionToAdd = new Transaction { Ticker = "MSFT", Shares = 1, Type = TransactionType.Buy };
 
-            TransactionResult result = _portfolio.AddTransaction(
-                transactionToAdd, 
-                null
-            );
+			TransactionResult result = _portfolio.AddTransaction(
+				transactionToAdd, 
+				null
+			);
 
-            Assert.AreEqual(false, result.IsSuccess);
-            Assert.AreEqual("Cannot work on a non-existing position.", result.Errors.FirstOrDefault());
-        }
+			Assert.AreEqual(false, result.IsSuccess);
+			Assert.AreEqual("Cannot work on a non-existing position.", result.Errors.FirstOrDefault());
+		}
 
-        [TestMethod]
-        public void CheckNotEnoughShareToSellError()
-        {
-            var transactionToAdd = new Transaction { Ticker = "YHOO", Shares = 5, Type = TransactionType.Sell };
-            TransactionResult result = _portfolio.AddTransaction(
-                transactionToAdd,
-                _portfolio.Positions.SingleOrDefault(pos => pos.Ticker == "YHOO")
-            );
+		[TestMethod]
+		public void CheckNotEnoughShareToSellError()
+		{
+			var transactionToAdd = new Transaction { Ticker = "YHOO", Shares = 5, Type = TransactionType.Sell };
+			TransactionResult result = _portfolio.AddTransaction(
+				transactionToAdd,
+				_portfolio.Positions.SingleOrDefault(pos => pos.Ticker == "YHOO")
+			);
 
-            Assert.AreEqual(false, result.IsSuccess);
-            Assert.AreEqual("Not enough shares for this position. Cannot sell.", result.Errors.FirstOrDefault());
-        }
-    }
+			Assert.AreEqual(false, result.IsSuccess);
+			Assert.AreEqual("Not enough shares for this position. Cannot sell.", result.Errors.FirstOrDefault());
+		}
+	}
 }
