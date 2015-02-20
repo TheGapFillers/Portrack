41c9cef508774bb5daebc2a600869c14c32091bd
Ambroise Couissin <ambroise.couissin@quantifeed.com>
2015-02-20 18:40:35 +0800

    A few tweeks. Not much

--- src/Portrack/Portrack/Controllers/Application/TransactionsController.cs
+++ src/Portrack/Portrack/Controllers/Application/TransactionsController.cs
@@ -9,143 +9,141 @@ using System.Web.Http;
 
 namespace Portrack.Controllers.Application
 {
-	/// <summary>
-	/// API class class against which all the transactions call are made.
-	/// All the calls in this class need authorization.
-	/// </summary>
-	[RoutePrefix("api/transactions")]
-	public class TransactionsController : BaseController
-	{
-		/// <summary>
-		/// Class constructor which injected 'IApplicationRepository' dependency.
-		/// </summary>
-		/// <param name="repository">Injected 'IApplicationRepository' dependency.</param>
-		/// <param name="provider">Injected 'IMarketDataProvider' dependency.</param>
-		public TransactionsController(IApplicationRepository repository, IMarketDataProvider provider)
-			: base(repository, provider)
-		{
-		}
+    /// <summary>
+    /// API class class against which all the transactions call are made.
+    /// All the calls in this class need authorization.
+    /// </summary>
+    [RoutePrefix("api/transactions")]
+    public class TransactionsController : BaseController
+    {
+        /// <summary>
+        /// Class constructor which injected 'IApplicationRepository' dependency.
+        /// </summary>
+        /// <param name="repository">Injected 'IApplicationRepository' dependency.</param>
+        /// <param name="provider">Injected 'IMarketDataProvider' dependency.</param>
+        public TransactionsController(IApplicationRepository repository, IMarketDataProvider provider)
+            : base(repository, provider)
+        {
+        }
 
-		/// <summary>
-		/// Get method to get the transactions of the current authenticated user.
-		/// </summary>
-		/// <param name="portfolioName">Comma-separated string of the portfolio name.</param>
-		/// <param name="tickers">Comma-separated string of the tickers wanted.</param>
-		/// <returns>Ok status with a list of transactions.</returns>
-		[Route("{portfolioName?}/{tickers?}")]
-		[HttpGet]
-		public async Task<IHttpActionResult> Get(string portfolioName = null, string tickers = null)
-		{
-			ICollection<Transaction> transactions;
-			if (string.IsNullOrWhiteSpace(portfolioName))
-			{
-				transactions = await _repository.GetTransactionsAsync(User.Identity.Name);
-				return Ok(transactions);
-			}
+        /// <summary>
+        /// Get method to get the transactions of the current authenticated user.
+        /// </summary>
+        /// <param name="portfolioName">Comma-separated string of the portfolio name.</param>
+        /// <param name="tickers">Comma-separated string of the tickers wanted.</param>
+        /// <returns>Ok status with a list of transactions.</returns>
+        [Route("{portfolioName?}/{tickers?}")]
+        [HttpGet]
+        public async Task<IHttpActionResult> Get(string portfolioName = null, string tickers = null)
+        {
+            ICollection<Transaction> transactions;
+            if (string.IsNullOrWhiteSpace(portfolioName))
+            {
+                transactions = await _repository.GetTransactionsAsync(User.Identity.Name);
+                return Ok(transactions);
+            }
 
-			if (string.IsNullOrWhiteSpace(tickers))
-			{
-				transactions = await _repository.GetTransactionsAsync(User.Identity.Name, portfolioName);
-				return Ok(transactions);
-			}
+            if (string.IsNullOrWhiteSpace(tickers))
+            {
+                transactions = await _repository.GetTransactionsAsync(User.Identity.Name, portfolioName);
+                return Ok(transactions);
+            }
 
-			IEnumerable<string> tickerEnum = tickers.Split(',').Select(s => s.Trim());
-			transactions = await _repository.GetTransactionsAsync(User.Identity.Name, portfolioName, tickerEnum);
-			return Ok(transactions);
-		}
+            IEnumerable<string> tickerEnum = tickers.Split(',').Select(s => s.Trim());
+            transactions = await _repository.GetTransactionsAsync(User.Identity.Name, portfolioName, tickerEnum);
+            return Ok(transactions);
+        }
 
 
-		/// <summary>
-		/// Post method to upload a new transacton for the current authenticated user.
-		/// </summary>
-		/// <param name="portfolio">The embodied transaction to upload.</param>
-		/// <returns>
-		///     Ok(createdPortfolio) if datalayer accepted transaction.
-		///     BadRequest(ModelState) if modelstate is invalid.
-		///     InternalServerError if DataMarkerProvider's response is invalid
-		/// </returns>
-		[Route("")]
-		[HttpPost]
-		public async Task<IHttpActionResult> Post([FromBody]Transaction transaction)
-		{
-			if (!ModelState.IsValid) return BadRequest(ModelState);
+        /// <summary>
+        /// Post method to upload a new transacton for the current authenticated user.
+        /// </summary>
+        /// <param name="portfolio">The embodied transaction to upload.</param>
+        /// <returns>
+        ///     Ok(createdPortfolio) if datalayer accepted transaction.
+        ///     BadRequest(ModelState) if modelstate is invalid.
+        ///     InternalServerError if DataMarkerProvider's response is invalid
+        /// </returns>
+        [Route("")]
+        [HttpPost]
+        public async Task<IHttpActionResult> Post([FromBody]Transaction transaction)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
 
-			//Todo: check if no quot at all to return properly
+            //Todo: check if no quot at all to return properly
 
-			// Get the portfolio represented by the PortfolioName in the posted transaction.
-			Portfolio portfolio = await _repository.GetPortfolioAsync(User.Identity.Name, transaction.PortfolioName, true, true);
-			IHttpActionResult portFolioError;
-			if ((portFolioError = checkForPortfolioError(portfolio, transaction)) != null)
-			{
-				return portFolioError;
-			}
-				
-			// Get the instrument represented by the Ticker in the posted transaction
-			Instrument instrument = await _repository.GetInstrumentAsync(transaction.Ticker);
-			if (instrument == null)
-			{
-				return Ok(TransactionResult.Failed(portfolio, null, transaction, 
-					string.Format("Instrument '{0}' doens't exist.", transaction.Ticker)));
-			}
-				
-			// Get the position associated position if it exists.
-			Position position = await _repository.GetPositionAsync(portfolio.UserName, portfolio.PortfolioName, instrument.Ticker);
-			if (position == null)
-			{
-				position = new Position(portfolio, instrument);
-				portfolio.Positions.Add(position);
-			}
+            // Get the portfolio represented by the PortfolioName in the posted transaction.
+            Portfolio portfolio = await _repository.GetPortfolioAsync(User.Identity.Name, transaction.PortfolioName, true, true);
+            IHttpActionResult portFolioError;
+            if ((portFolioError = CheckForPortfolioError(portfolio, transaction)) != null)
+            {
+                return portFolioError;
+            }
 
-			try
-			{
-				transaction = await retrieveTransactionPriceAsync(transaction, instrument);
-			}
-			catch (Exception ex)
-			{
-				return InternalServerError(ex);
-			}
-			
-			// Add the transaction and get the transaction result.
-			TransactionResult result = portfolio.AddTransaction(transaction, position);
-			clearPositionIfNoMoreShares(result);
-			// Send the changes made in the data layer to the database and return the transaction results.
-			await _repository.SaveAsync();
-			return Ok(result);
-		}
+            // Get the instrument represented by the Ticker in the posted transaction
+            Instrument instrument = await _repository.GetInstrumentAsync(transaction.Ticker);
+            if (instrument == null)
+            {
+                return Ok(TransactionResult.Failed(portfolio, null, transaction,
+                    string.Format("Instrument '{0}' doens't exist.", transaction.Ticker)));
+            }
 
-		private IHttpActionResult checkForPortfolioError(Portfolio portfolio, Transaction transaction)
-		{
-			IHttpActionResult retVal = null;
-			if (portfolio == null)
-			{
-				return Ok(TransactionResult.Failed(null, null, transaction, 
-					string.Format("Portfolio '{0}' | '{1}' not found.", User.Identity.Name, transaction.PortfolioName)));
-			} else if (portfolio.Positions == null)
-			{
-				retVal = InternalServerError(new Exception("Positions loading for portfolio failed."));
-			} else if (portfolio.Transactions == null)
-			{
-				retVal = InternalServerError(new Exception("Transactions loading for portfolio failed."));
-			}
+            // Get the position associated position if it exists.
+            Position position = await _repository.GetPositionAsync(portfolio.UserName, portfolio.PortfolioName, instrument.Ticker);
+            if (position == null)
+            {
+                position = new Position(portfolio, instrument);
+                portfolio.Positions.Add(position);
+            }
 
-			return retVal;
-		}
+            transaction = await RetrieveTransactionPriceAsync(transaction, instrument);
 
-		private async Task<Transaction> retrieveTransactionPriceAsync(Transaction transaction, Instrument instrument) {
-			ICollection<Quote> quotes = await _provider.GetHistoricalPricesAsync(new List<String> {transaction.Ticker}, transaction.Date, transaction.Date);
-			
-			instrument.Quote = quotes.SingleOrDefault();
+            // Add the transaction and get the transaction result.
+            TransactionResult result = portfolio.AddTransaction(transaction, position);
+            ClearPositionIfNoMoreShares(result);
 
-			transaction.Price = instrument.Quote.Last * transaction.Shares;
-			return transaction;
-		}
+            // Send the changes made in the data layer to the database and return the transaction results.
+            await _repository.SaveAsync();
+            return Ok(result);
+        }
 
-		private void clearPositionIfNoMoreShares(TransactionResult result) {
-			if (result.IsSuccess && result.Position.Shares == 0)
-			{
-				_repository.DeletePositionAsync(result.Position);
-			}
-		}
+        private IHttpActionResult CheckForPortfolioError(Portfolio portfolio, Transaction transaction)
+        {
+            IHttpActionResult retVal = null;
+            if (portfolio == null)
+            {
+                return Ok(TransactionResult.Failed(null, null, transaction,
+                    string.Format("Portfolio '{0}' | '{1}' not found.", User.Identity.Name, transaction.PortfolioName)));
+            }
+            else if (portfolio.Positions == null)
+            {
+                retVal = InternalServerError(new Exception("Positions loading for portfolio failed."));
+            }
+            else if (portfolio.Transactions == null)
+            {
+                retVal = InternalServerError(new Exception("Transactions loading for portfolio failed."));
+            }
 
-	}
+            return retVal;
+        }
+
+        private async Task<Transaction> RetrieveTransactionPriceAsync(Transaction transaction, Instrument instrument)
+        {
+            ICollection<Quote> quotes = await _provider.GetHistoricalPricesAsync(new List<String> { transaction.Ticker }, transaction.Date, transaction.Date);
+
+            instrument.Quote = quotes.SingleOrDefault();
+
+            transaction.Price = instrument.Quote.Last * transaction.Shares;
+            return transaction;
+        }
+
+        private void ClearPositionIfNoMoreShares(TransactionResult result)
+        {
+            if (result.IsSuccess && result.Position.Shares == 0)
+            {
+                _repository.DeletePositionAsync(result.Position);
+            }
+        }
+
+    }
 }
--- src/Portrack/Portrack/Models/Application/Portfolio.cs
+++ src/Portrack/Portrack/Models/Application/Portfolio.cs
@@ -5,94 +5,96 @@ using System.Linq;
 
 namespace Portrack.Models.Application
 {
-    public class Portfolio
-    {
-        [JsonIgnore]
-        public int PortfolioId { get; set; }
-        public string PortfolioName { get; set; }
+	public class Portfolio
+	{
+		[JsonIgnore]
+		public int PortfolioId { get; set; }
+		public string PortfolioName { get; set; }
 
 
-        public string UserName { get; set; }
+		public string UserName { get; set; }
 
-        [JsonIgnore]
-        public ICollection<Position> Positions { get; set; }
-        [JsonIgnore]
-        public ICollection<Transaction> Transactions { get; set; }
+		[JsonIgnore]
+		public ICollection<Position> Positions { get; set; }
+		[JsonIgnore]
+		public ICollection<Transaction> Transactions { get; set; }
 
-        public PortfolioData PortfolioData { get; set; }
+		public PortfolioData PortfolioData { get; set; }
 
 
-        /// <summary>
-        /// Method of the portfolio class to add a transaction.
-        /// </summary>
-        /// <param name="transaction">The transaction to execute.</param>
-        /// <param name="position">The current position against which the transaction will be executed.</param>
-        /// <returns>The transaction results.</returns>
-        public TransactionResult AddTransaction(Transaction transaction, Position position)
-        {
+		/// <summary>
+		/// Method of the portfolio class to add a transaction.
+		/// </summary>
+		/// <param name="transaction">The transaction to execute.</param>
+		/// <param name="position">The current position against which the transaction will be executed.</param>
+		/// <returns>The transaction results.</returns>
+		public TransactionResult AddTransaction(Transaction transaction, Position position)
+		{
 			if (position == null)
-            {
-                return TransactionResult.Failed(
+			{
+				return TransactionResult.Failed(
 					this, position, transaction, "Cannot work on a non-existing position."
 				);
-            }
-            if (transaction.Type == TransactionType.Sell)
-            {
-                if (transaction.Shares > position.Shares)
-                {
-                    return TransactionResult.Failed(
+			}
+			if (transaction.Type == TransactionType.Sell)
+			{
+				if (transaction.Shares > position.Shares)
+				{
+					return TransactionResult.Failed(
 						this, position, transaction,
-                        "Not enough shares for this position. Cannot sell."
+						"Not enough shares for this position. Cannot sell."
 					);
-                }
-                position.Shares -= transaction.Shares;
-            } else if (transaction.Type == TransactionType.Buy)
-            {
-                position.Shares += transaction.Shares;
-			} else //Should never happen
+				}
+				position.Shares -= transaction.Shares;
+			} 
+			else if (transaction.Type == TransactionType.Buy)
+			{
+				position.Shares += transaction.Shares;
+			} 
+			else //Should never happen
 			{
 				 return TransactionResult.Failed(
 					this, position, transaction,
-                    "Unknown transaction type."
+					"Unknown transaction type."
 				);
 			}
 
-            transaction.Ticker = transaction.Ticker.ToUpperInvariant();
+			transaction.Ticker = transaction.Ticker.ToUpperInvariant();
 
-            Transactions = Transactions ?? new List<Transaction>();
-            Transactions.Add(transaction);
+			Transactions = Transactions ?? new List<Transaction>();
+			Transactions.Add(transaction);
 
-            return TransactionResult.Succeeded(this, position, transaction);
-        }
-    }
+			return TransactionResult.Succeeded(this, position, transaction);
+		}
+	}
 
-    public class PortfolioData
-    {
-        public decimal CostBasis { get; set; }
-        public decimal MarketValue { get; set; }
-        public decimal Gain { get { return MarketValue - CostBasis; } }
-        public double GainPercentage { get { return CostBasis != 0 ? (double)(MarketValue / CostBasis) : 0; } }
-        public double OverallReturn { get; set; }
-    }
-
-
+	public class PortfolioData
+	{
+		public decimal CostBasis { get; set; }
+		public decimal MarketValue { get; set; }
+		public decimal Gain { get { return MarketValue - CostBasis; } }
+		public double GainPercentage { get { return CostBasis != 0 ? (double)(MarketValue / CostBasis) : 0; } }
+		public double OverallReturn { get; set; }
+	}
 
 
 
-    public class PortfolioException : Exception
-    {
-        public PortfolioException()
-        {
-        }
 
-        public PortfolioException(string message)
-            : base(message)
-        {
-        }
 
-        public PortfolioException(string message, Exception inner)
-            : base(message, inner)
-        {
-        }
-    }
+	public class PortfolioException : Exception
+	{
+		public PortfolioException()
+		{
+		}
+
+		public PortfolioException(string message)
+			: base(message)
+		{
+		}
+
+		public PortfolioException(string message, Exception inner)
+			: base(message, inner)
+		{
+		}
+	}
 }
