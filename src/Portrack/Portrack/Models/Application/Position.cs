f038c9c03f4ab2fc5a6f5a9eb90e1c9893d8eed3
Ambroise Couissin <ambroise.couissin@gmail.com>
2015-02-21 18:10:28 +0800

    Optimized position data calculation design and calls. Calls to repository and data provider remain in controller, but calculation is done is PositionData class.

--- src/Portrack/Portrack/Models/Application/Position.cs
+++ src/Portrack/Portrack/Models/Application/Position.cs
@@ -3,41 +3,56 @@ using System.Collections.Generic;
 
 namespace Portrack.Models.Application
 {
-    public class Position
-    {
-        public Position()
-        {
+	public class Position
+	{
+		public Position()
+		{
 
-        }
-
-        public Position(Portfolio portfolio, Instrument instrument, int shareAmount = 0)
-        {
-            Portfolio = portfolio;
-            Instrument = instrument;
-            Shares = shareAmount;
-        }
-
-        [JsonIgnore]
-        public int PositionId { get; set; }
-        [JsonIgnore]
-        public Portfolio Portfolio { get; set; }
-        public Instrument Instrument { get; private set; }
-        public string Ticker { get { return Instrument != null ? Instrument.Ticker : string.Empty; } }
-        public int Shares { get; set; }
-        public PositionData PositionData { get; set; }
-    }
-
-    public class PositionData
-    {
-        public PositionData(decimal costBasis, decimal marketValue) {
-			CostBasis = costBasis;
-			MarketValue = marketValue;
 		}
 
-        public decimal CostBasis { get; set; }
-        public decimal MarketValue { get; set; }
-        public decimal Gain { get { return MarketValue - CostBasis; } }
-        public double GainPercentage { get { return CostBasis != 0 ? (double)(MarketValue / CostBasis) : 0; } }
-        public double OverallReturn { get; set; }
-    }
+		public Position(Portfolio portfolio, Instrument instrument, int shareAmount = 0)
+		{
+			Portfolio = portfolio;
+			Instrument = instrument;
+			Shares = shareAmount;
+		}
+
+		[JsonIgnore]
+		public int PositionId { get; set; }
+		[JsonIgnore]
+		public Portfolio Portfolio { get; set; }
+		public Instrument Instrument { get; private set; }
+		public string Ticker { get { return Instrument != null ? Instrument.Ticker : string.Empty; } }
+		public int Shares { get; set; }
+		public PositionData PositionData { get; set; }
+
+		public void SetPositionData(IEnumerable<Transaction> transactions, Quote quote)
+		{
+			PositionData = new PositionData
+			{
+				CostBasis = CalculateCostBasis(transactions),
+				MarketValue = quote.Last * Shares
+			};
+		}
+
+
+		private decimal CalculateCostBasis(IEnumerable<Transaction> transactions)
+		{
+			decimal retVal = 0.0m;
+			foreach (Transaction t in transactions)
+			{
+				retVal += t.Price;
+			}
+			return retVal;
+		}
+	}
+
+	public class PositionData
+	{
+		public decimal CostBasis { get; set; }
+		public decimal MarketValue { get; set; }
+		public decimal Gain { get { return MarketValue - CostBasis; } }
+		public double GainPercentage { get { return CostBasis != 0 ? (double)(MarketValue / CostBasis) : 0; } }
+		public double OverallReturn { get; set; }
+	}
 }
