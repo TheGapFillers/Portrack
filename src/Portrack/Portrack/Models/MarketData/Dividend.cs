aefda2d2b037315188827bcce0b15d45f3be66d4
Ambroise Couissin <ambroise.couissin@gmail.com>
2015-02-24 02:59:34 +0800

    - Little update of dividend generation

--- src/Portrack/Portrack/Models/MarketData/Dividend.cs
+++ src/Portrack/Portrack/Models/MarketData/Dividend.cs
@@ -7,5 +7,7 @@ namespace Portrack.Models.MarketData
 {
     public class Dividend : MarketDataBase
     {
+        public DateTime Date { get; set; }
+        public decimal Amount { get; set; }
     }
 }
