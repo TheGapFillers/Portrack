00c4e08d0335f5a016c30ec6a87a3c31efb714d0
Valter Santos Matos <valter.santos.matos@gmail.com>
2015-04-06 20:35:19 +0800

    Clean front end with template

---
+++ src/TheGapFillers.Portrack/TheGapFillers.Portrack.WebSite/Gruntfile.js
@@ -0,0 +1,46 @@
+﻿module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        bower_concat: {
+            all: {
+                dest: 'js/bower.js',
+                cssDest: 'js/bower.css',
+                exclude: [
+                    'datatables-plugins'
+                ]
+            },
+
+        },
+        uglify: {
+            bower: {
+                options: {
+                    mangle: true,
+                    compress: true
+                },
+                files: {
+                    'js/bower.min.js': 'js/bower.js'
+                }
+            }
+        },
+        cssmin: {
+            options: {
+                shorthandCompacting: false,
+                roundingPrecision: -1
+            },
+            target: {
+                files: {
+                    'js/bower.min.css': ['js/bower.css']
+                }
+            }
+        }
+    });
+
+    // Load the plugin that provides the "uglify" task.
+    require('load-grunt-tasks')(grunt);
+
+    // Default task(s).
+    grunt.registerTask('default', ['bower_concat', 'uglify', 'cssmin']);
+
+};
